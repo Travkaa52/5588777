b9dbe42db2a8ec49c4229da0d9ec233ae075f10e
Travkaa52 <dimakasan028@gmail.com>
2026-02-01 17:38:53 +0200

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,17 @@
+const CACHE_NAME = 'hud-v3-cache';
+const ASSETS = [
+    './',
+    './index.html',
+    './app.js',
+    'https://cdn.tailwindcss.com',
+    'https://unpkg.com/leaflet@1.9.4/dist/leaflet.css',
+    'https://unpkg.com/leaflet@1.9.4/dist/leaflet.js'
+];
+
+self.addEventListener('install', e => {
+    e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+});
+
+self.addEventListener('fetch', e => {
+    e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+});
